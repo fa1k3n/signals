48f0a235a666c0a4d66ae6ebe2060807b39ac5b3
Falken <johan.falkenstrom@gmail.com>
2018-04-03 06:33:40 +0200

    more test cases

--- test/tst_signals.cpp
+++ test/tst_signals.cpp
@@ -3,15 +3,19 @@
 
 #include <signals/signal.hpp>
 
+#include <string>
+
 using namespace testing;
 
 class testEmitter {
 public:
 	MOCK_METHOD0(noArgsSlot, void());
+    MOCK_METHOD2(twoArgsSlot, void(int, std::string));
 	Signals::signal<> noArgSignal;
+    Signals::signal<int, std::string> twoArgSignal;
 };
 
-TEST(line, basicLambdaConnect) {
+TEST(signal, basicLambdaConnect) {
 	testEmitter tst;
 	bool hasBeenCalled = false;
 	Signals::connect(&testEmitter::noArgSignal, &tst, [&hasBeenCalled]() {
@@ -21,7 +25,7 @@ TEST(line, basicLambdaConnect) {
 	EXPECT_TRUE(hasBeenCalled);
 }
 
-TEST(line, basicMethodConnect) {
+TEST(signal, basicMethodConnect) {
 	testEmitter tst;
 	EXPECT_CALL(tst, noArgsSlot());
 	Signals::connect(&testEmitter::noArgSignal, &tst, &testEmitter::noArgsSlot, &tst);
@@ -33,10 +37,17 @@ void fun() {
 	funCalled = true;
 }
 
-TEST(line, basicFunctionConnect) {
+TEST(signal, basicFunctionConnect) {
 	testEmitter tst;
 	Signals::connect(&testEmitter::noArgSignal, &tst, &fun);
 	emit tst.noArgSignal();
 	EXPECT_TRUE(funCalled);
+}
+
+TEST(signal, twoArgumentConnect) {
+    testEmitter tst;
+    EXPECT_CALL(tst, twoArgsSlot(1, "foo bar"));
+    Signals::connect(&testEmitter::twoArgSignal, &tst, &testEmitter::twoArgsSlot, &tst);
+    emit tst.twoArgSignal(1, "foo bar");
 }
 
